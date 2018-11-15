c5c7ef1ec53eca6ad129a8d98f5d9a57c4462ed8
zwb <12345@qq.com>
2018-11-15 11:22:03 +0800

    第三次修改

--- Test02/src/day01/HelloWorld.java
+++ Test02/src/day01/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world !");
 		System.out.println("123");
+		System.out.println("1234");
+		System.out.println("12345");
 	}
 }
